020b0a0c95ca83bad1928c0b28fff805ab9bc153
rdrgox <rdrgox@gmail.com>
2020-01-15 23:10:01 -0300

    se agrega clase helper

---
+++ AppWebMvc/Helper/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace AppWebMvc.Helper
+{
+    public class StudentApi
+    {
+        public HttpClient Initial()
+        {
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://localhost:44379/");
+
+            return client;
+        }
+    }
+}
