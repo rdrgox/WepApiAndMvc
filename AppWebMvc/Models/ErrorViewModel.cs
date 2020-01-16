7d99fc544d234582c9d0814b06f8889e40bc4697
rdrgox <rdrgox@gmail.com>
2020-01-15 22:36:51 -0300

    app web mvc

---
+++ AppWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
