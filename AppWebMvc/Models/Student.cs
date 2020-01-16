e125a37cf12eb43fc52f7b852555c64f3dd60131
rdrgox <rdrgox@gmail.com>
2020-01-15 23:09:43 -0300

    se agregar clase student en mvc

---
+++ AppWebMvc/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AppWebMvc.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public String Direccion { get; set; }
+
+    }
+}
