3f3b2d1d034dd3988e8ef595ddc656516f27b714
rdrgox <rdrgox@gmail.com>
2020-01-22 00:12:42 -0300

    Post delete

--- ApiCRUD/Controllers/StudentController.cs
+++ ApiCRUD/Controllers/StudentController.cs
@@ -4,6 +4,7 @@ namespace ApiCRUD.Controllers
 {
     using System.Collections.Generic;
     using System.Linq;
+    using System.Threading.Tasks;
     using ApiCRUD.Data;
     using ApiCRUD.Models;
     using Microsoft.AspNetCore.Mvc;
@@ -26,11 +27,42 @@ namespace ApiCRUD.Controllers
             return _context.students.ToList();
         }
 
+        //Get one Student
         [HttpGet("{id}")]
         public Student GetStudent(int Id)
         {
             var student = _context.students.Where(a => a.Id == Id).SingleOrDefault();
             return student;
+        }
+
+        [HttpPost]
+        public IActionResult PostStudent([FromBody]Student student)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("model no es valido");
+
+            _context.students.Add(student);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        //Post delete
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var student = await _context.students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
         }
 
 
